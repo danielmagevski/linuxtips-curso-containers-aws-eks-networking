1bd235ccf37daacbb1950d55a356537380a1e22c
Daniel Magevski <daniel_magevski@hotmail.com>
2025-04-21 18:37:11 -0300

    aula networking

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,19 @@
-resource "aws_ssm_parameter" "vpc" {
-  name = "/${var.project_name}/vpc/id"
-  type = "String"
-
-  value = aws_vpc.main.id
+output "vpc_id" {
+  description = "SSM Parameter com o valor do vpc_id"
+  value       = aws_ssm_parameter.vpc.id
 }
 
-resource "aws_ssm_parameter" "public_subnets" {
-  count = length(aws_subnet.public)
-
-  name  = "/${var.project_name}/subnets/public/${var.public_subnets[count.index].availability_zone}/${var.public_subnets[count.index].name}"
-  type  = "String"
-  value = aws_subnet.public[count.index].id
+output "public_subnets" {
+  description = "SSM Parameters com os valores dos ID's das Subnets Públicas"
+  value       = aws_ssm_parameter.public_subnets[*].id
 }
 
-resource "aws_ssm_parameter" "private_subnets" {
-  count = length(aws_subnet.private)
-
-  name  = "/${var.project_name}/subnets/private/${var.private_subnets[count.index].availability_zone}/${var.private_subnets[count.index].name}"
-  type  = "String"
-  value = aws_subnet.private[count.index].id
+output "private_subnets" {
+  description = "SSM Parameters com os valores dos ID's das Subnets Privadas"
+  value       = aws_ssm_parameter.private_subnets[*].id
 }
 
-resource "aws_ssm_parameter" "databases_subnets" {
-  count = length(aws_subnet.database)
-
-  name  = "/${var.project_name}/subnets/databases/${var.database_subnets[count.index].availability_zone}/${var.database_subnets[count.index].name}"
-  type  = "String"
-  value = aws_subnet.database[count.index].id
+output "database_subnets" {
+  description = "SSM Parameters com os valores dos ID's das Subnets de Databases"
+  value       = aws_ssm_parameter.databases_subnets[*].id
 }
